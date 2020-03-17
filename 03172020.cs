6850f3f10df7b3abcaf124675bda6f7d4cf3eb06
Os1risDem14n <48409985+Os1risDem14n@users.noreply.github.com>
2020-03-17 11:21:29 +0700

    Update 03172020.cs

--- 03172020.cs
+++ 03172020.cs
@@ -6,38 +6,126 @@ using System.Threading.Tasks;
 
 namespace BankAccount
 {
-	class StartUp
-	{
-		public class BankAccount 
-		{
-			// Private modifier
-			private int id;
-			private decimal balance;
-			// Public modifier
-			public int Id;
-			public decimal Balance;
-						
-			public void Deposit(int depositAmount)
-			{
-				Balance = Balance + depositAmount;
-			}
+    public class BankAccount
+    {
+        private int id;
+        private decimal balance;
+        public BankAccount() : this(0, 0)
+        {
+        }
+        public BankAccount(int id) : this(id, 0)
+        {
+        }
+        public BankAccount(decimal balance) : this(0, balance)
+        {
+        }
+        public BankAccount(int id, decimal balance)
+        {
+            this.id = id;
+            this.balance = balance;
+        }
+        public int ID
+        {
 
-			public void WithDraw(int withdrawAmount)
-			{
-				Balance = Balance - withdrawAmount;
-			}
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public decimal Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+        public void Deposit(decimal amount)
+        {
+            this.balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            this.balance -= amount;
+        }
+        public override string ToString()
+        {
+            return $"Account ID{this.id}, balance {this.balance:F2}";
+        }
+        public static void Main()
+        {
+            var accounts = new Dictionary<int, BankAccount>();
+            var input = Console.ReadLine();
+            while (input != "End")
+            {
+                var inputData = input.Split();
 
-		}
-		static void Main(string[] args)
-		{
-			BankAccount acc = new BankAccount();
-			acc.id = 1;
-			acc.balance = 15;
-			Console.WriteLine($"Account {acc.id}, balance {acc.balance}");
-			acc.Deposit(10);
-			Console.WriteLine($"Account {acc.id}, balance {acc.balance}");
-			acc.WithDraw(15);
-			Console.WriteLine($"Account {acc.id}, balance {acc.balance}");
-		}
-	}
+                switch (inputData[0])
+                {
+                    case "Create":
+                        CreateAccount(int.Parse(inputData[1]), accounts);
+                        break;
+                    case "Deposit":
+                        Deposit(int.Parse(inputData[1]), decimal.Parse(inputData[2]), accounts);
+                        break;
+                    case "Withdraw":
+                        Withdraw(int.Parse(inputData[1]), decimal.Parse(inputData[2]), accounts);
+                        break;
+                    case "Print":
+                        Print(int.Parse(inputData[1]), accounts);
+                        break;
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void Print(int id, Dictionary<int, BankAccount> accounts)
+        {
+            if (!accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+                return;
+            }
+
+            Console.WriteLine(accounts[id].ToString());
+        }
+
+        private static void Withdraw(int id, decimal amount, Dictionary<int, BankAccount> accounts)
+        {
+            if (!accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+                return;
+            }
+
+            if (accounts[id].Balance < amount)
+            {
+                Console.WriteLine("Insufficient balance");
+                return;
+            }
+
+            accounts[id].Withdraw(amount);
+        }
+
+        private static void Deposit(int id, decimal amount, Dictionary<int, BankAccount> accounts)
+        {
+            if (!accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+                return;
+            }
+
+            accounts[id].Deposit(amount);
+        }
+
+        private static void CreateAccount(int id, Dictionary<int, BankAccount> accounts)
+        {
+            if (accounts.ContainsKey(id))
+            {
+                Console.WriteLine("Account already exists");
+                return;
+            }
+
+            accounts[id] = new BankAccount(id);
+        }
+    }
 }
+
