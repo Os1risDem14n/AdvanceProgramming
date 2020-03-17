a1d4542c792388fff2181a40e3b2aa3b6ee359dc
Os1risDem14n <48409985+Os1risDem14n@users.noreply.github.com>
2020-03-17 09:12:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+	class StartUp
+	{
+		public class BankAccount 
+		{
+			public int id;
+			public decimal balance;
+						
+			public void Deposit(int zzz)
+			{
+				balance=balance+zzz;
+			}
+
+			public void WithDraw(int zzz)
+			{
+				balance=balance-zzz;
+			}
+
+		}
+		static void Main(string[] args)
+		{
+			BankAccount acc = new BankAccount();
+            acc.id = 1;
+            acc.balance = 15;
+            Console.WriteLine($"Account {acc.id}, balance {acc.balance}");
+            acc.Deposit(10);
+            Console.WriteLine($"Account {acc.id}, balance {acc.balance}");
+            acc.WithDraw(15);
+            Console.WriteLine($"Account {acc.id}, balance {acc.balance}");
+		}
+	}
+}
